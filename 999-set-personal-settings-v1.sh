ace1627d29ac25506cb0e44c9955f5058a7fecb3
bdelonglee <bdelonglee@gmail.com>
2018-01-02 00:00:38 -0500

    lightdm and slim background, icons. wallpaper selecion > selection

--- 999-set-personal-settings-v1.sh
+++ 999-set-personal-settings-v1.sh
@@ -19,15 +19,13 @@ echo "Changing Shell to ZSH"
 sudo chsh ben -s /bin/zsh
 
 echo "Setting Lightdm-webkit2 Wallpaper"
-sudo mv /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background_backup.jpg
-
+sudo cp /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background_backup.jpg
 sudo cp Personal/wallpapers/selection_001/aerial_cloud_blue_cyan_001.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg
+sudo cp default_user.png /usr/share/lightdm-webkit/themes/litarvan/images/default_user.png
 
 echo "Setting Slim Lock Screen Theme"
 sudo cp -r Personal/themes/slim/perso_aerial-water-dark-blue-001 /usr/share/slim/themes/
-
 sudo mv /etc/slim.conf /etc/slim.conf_bkp
-
 sudo cp Personal/themes/slim/slim.conf /etc/slim.conf
 
 echo ""
@@ -36,4 +34,3 @@ echo "###### Personal Settings ########################"
 echo "###### 999 END ##################################"
 echo "#################################################"
 echo ""
-
