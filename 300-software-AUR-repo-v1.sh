8b41e63752b4b0cef0018aba66693abd87551cbc
bdelonglee <bdelonglee@gmail.com>
2018-02-08 13:50:33 -0500

    zsh-syntax-highlight

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -95,6 +95,7 @@ sh AUR/install-ttf-mac-fonts-v*.sh
 sh AUR/install-unclutter-xfixes-git-v*.sh
 sh AUR/install-virtualbox-for-linux-kernel-v*.sh
 sh AUR/install-xcursor-breeze-v*.sh
+sh AUR/install-zsh-syntax-highlighting-git-v*.sh
 
 #Other possibilities
 #sh AUR/install-canon-mg7750-v*.sh
